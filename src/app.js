2ed173e361f59a6f69bc82446d223c48eabb08b8
gaurav060404 <gs762282@gmail.com>
2026-01-26 22:07:12 +0530

    initial project setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import ApiResponse from "./utils/ApiResponse.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200,null,"This is backend for Acquisitions"));
+});
+
+export default app;
